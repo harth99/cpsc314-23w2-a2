210faca1240742054801de393c8bc439d57e300a
Jiayu Huang <harthuang990517@gmail.com>
2024-02-12 22:49:27 -0800

    part a done

--- A2.js
+++ A2.js
@@ -3,19 +3,14 @@
  * Assignment 2 Template
  */
 
-import { setup, loadAndPlaceGLB } from './js/setup.js';
-import * as THREE from './js/three.module.js';
-import { SourceLoader } from './js/SourceLoader.js';
-import { THREEx } from './js/KeyboardState.js';
+import { setup, loadAndPlaceGLB } from "./js/setup.js";
+import * as THREE from "./js/three.module.js";
+import { SourceLoader } from "./js/SourceLoader.js";
+import { THREEx } from "./js/KeyboardState.js";
 
 // Setup and return the scene and related objects.
 // You should look into js/setup.js to see what exactly is done here.
-const {
-  renderer,
-  scene,
-  camera,
-  worldFrame,
-} = setup();
+const { renderer, scene, camera, worldFrame } = setup();
 
 // Used THREE.Clock for animation
 var clock = new THREE.Clock();
@@ -28,13 +23,13 @@ var clock = new THREE.Clock();
 
 // As in A1 we position the sphere in the world solely using this uniform
 // So the initial y-offset being 1.0 here is intended.
-const sphereOffset = { type: 'v3', value: new THREE.Vector3(0.0, 1.0, 0.0) };
+const sphereOffset = { type: "v3", value: new THREE.Vector3(0.0, 1.0, 0.0) };
 
 // Distance threshold beyond which the armadillo should shoot lasers at the sphere (needed for Q1c).
 const LaserDistance = 10.0;
 
 // Waving threshold distance
-const waveDistance = 5.0
+const waveDistance = 5.0;
 
 // TODO: you may want to add more const's or var's to implement
 // the armadillo waving its hand
@@ -43,7 +38,7 @@ const waveDistance = 5.0
 const sphereMaterial = new THREE.ShaderMaterial({
   uniforms: {
     sphereOffset: sphereOffset,
-  }
+  },
 });
 
 const eyeMaterial = new THREE.ShaderMaterial();
@@ -52,21 +47,28 @@ const eyeMaterial = new THREE.ShaderMaterial();
 
 // Load shaders.
 const shaderFiles = [
-  'glsl/sphere.vs.glsl',
-  'glsl/sphere.fs.glsl',
-  'glsl/eye.vs.glsl',
-  'glsl/eye.fs.glsl',
+  "glsl/sphere.vs.glsl",
+  "glsl/sphere.fs.glsl",
+  "glsl/eye.vs.glsl",
+  "glsl/eye.fs.glsl",
 ];
 
 new SourceLoader().load(shaderFiles, function (shaders) {
-  sphereMaterial.vertexShader = shaders['glsl/sphere.vs.glsl'];
-  sphereMaterial.fragmentShader = shaders['glsl/sphere.fs.glsl'];
+  sphereMaterial.vertexShader = shaders["glsl/sphere.vs.glsl"];
+  sphereMaterial.fragmentShader = shaders["glsl/sphere.fs.glsl"];
 
-  eyeMaterial.vertexShader = shaders['glsl/eye.vs.glsl'];
-  eyeMaterial.fragmentShader = shaders['glsl/eye.fs.glsl'];
+  eyeMaterial.vertexShader = shaders["glsl/eye.vs.glsl"];
+  eyeMaterial.fragmentShader = shaders["glsl/eye.fs.glsl"];
 });
 
 // TODO: Load and place the Armadillo geometry in GLB format
+loadAndPlaceGLB("glb/armadillo.glb", function (armadillo) {
+  armadillo.scene.position.set(0.0, 5.3, -8.0);
+  armadillo.scene.rotation.y = Math.PI;
+  armadillo.scene.scale.set(0.1, 0.1, 0.1);
+  armadillo.scene.parent = worldFrame;
+  scene.add(armadillo.scene);
+});
 
 // Create the main sphere geometry
 // https://threejs.org/docs/#api/en/geometries/SphereGeometry
@@ -96,32 +98,29 @@ scene.add(leftEyeSocket);
 const keyboard = new THREEx.KeyboardState();
 
 function checkKeyboard() {
-  if (keyboard.pressed("W"))
-    sphereOffset.value.z -= 0.1;
-  else if (keyboard.pressed("S"))
-    sphereOffset.value.z += 0.1;
+  if (keyboard.pressed("W")) sphereOffset.value.z -= 0.1;
+  else if (keyboard.pressed("S")) sphereOffset.value.z += 0.1;
 
-  if (keyboard.pressed("A"))
-    sphereOffset.value.x -= 0.1;
-  else if (keyboard.pressed("D"))
-    sphereOffset.value.x += 0.1;
+  if (keyboard.pressed("A")) sphereOffset.value.x -= 0.1;
+  else if (keyboard.pressed("D")) sphereOffset.value.x += 0.1;
 
-  if (keyboard.pressed("E"))
-    sphereOffset.value.y -= 0.1;
-  else if (keyboard.pressed("Q"))
-    sphereOffset.value.y += 0.1;
+  if (keyboard.pressed("E")) sphereOffset.value.y -= 0.1;
+  else if (keyboard.pressed("Q")) sphereOffset.value.y += 0.1;
 
   // The following tells three.js that some uniforms might have changed.
   sphereMaterial.needsUpdate = true;
   eyeMaterial.needsUpdate = true;
 
   // Move the sphere light in the scene. This allows the floor to reflect the light as it moves.
-  sphereLight.position.set(sphereOffset.value.x, sphereOffset.value.y, sphereOffset.value.z);
+  sphereLight.position.set(
+    sphereOffset.value.x,
+    sphereOffset.value.y,
+    sphereOffset.value.z
+  );
 }
 
 // Setup update callback
 function update() {
-
   // TODO: make neccesary changes to implement gazing, the armadillo waving its hand, etc.
   checkKeyboard();
 
